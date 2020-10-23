08c40de25bb24a96afa6cc9ebadbdb3847ebab60
Eugene Slugin <36126850+Stoikiy@users.noreply.github.com>
2020-10-23 17:44:13 +0300

    Update app.js
    deleting depricated var, add let \ const

--- javaScript/javaScript/app.js
+++ javaScript/javaScript/app.js
@@ -1,51 +1,55 @@
-var myNodelist = document.getElementsByTagName('LI');
- var i;
-for (i=0; i <myNodelist.length; i++){
-    var span = document.createElement("SPAN");
-    var txt = document.createTextNode("\u00D7");
+let nodeList = document.getElementsByTagName('LI');
+let close = document.getElementsByClassName("close");
+let list = document.querySelector('ul');
+
+
+for (let i=0; i < nodeList.length; i++){
+    let span = document.createElement("SPAN");
+    const txt = document.createTextNode("\u00D7");
+
     span.className = "close";
     span.appendChild(txt);
-    myNodelist[i].appendChild(span);
+    nodeList[i].appendChild(span);
 }
 
-var close = document.getElementsByClassName("close");
-var i;
-for (i=0; i <close.length; i++){
+for (let i=0; i < close.length; i++){
     close[i].onclick = function () {
-        var div = this.parentElement;
+        const div = this.parentElement;
         div.style.display = "none";
-
     }
 }
 
-var list = document.querySelector('ul');
 list.addEventListener('click', function (ev) {
-if (ev.target.tagName === 'LI') {
-    ev.target.classList.toggle('checked');
-}
+    if (ev.target.tagName === 'LI') {
+        ev.target.classList.toggle('checked');
+    }
 },false);
 
 function newElement() {
-    var li = document.createElement("li");
-    var inputValue = document.getElementById("myInput").value;
-    var t = document.createTextNode(inputValue);
+    const li = document.createElement("li");
+    const inputValue = document.getElementById("myInput").value;
+    const t = document.createTextNode(inputValue);
+    
     li.appendChild(t);
+    
     if (inputValue === ''){
         alert("Привет ,привет JS!");
-        }else {
+    }else {
         document.getElementById("myUL").appendChild(li);
     }
+    
     document.getElementById("myInput").value = "";
 
-    var span = document.createElement("SPAN");
-    var txt = document.createTextNode("\u00D7");
+    const span = document.createElement("SPAN");
+    const txt = document.createTextNode("\u00D7");
+    
     span.className = "close";
     span.appendChild(txt);
     li.appendChild(span);
 
-    for (i = 0; i < close.length; i++) {
+    for (let i = 0; i < close.length; i++) {
         close[i].onclick = function () {
-            var div = this.parentElement;
+            const div = this.parentElement;
             div.style.display = "none";
         }
     }
